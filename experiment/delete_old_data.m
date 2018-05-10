128751539cf1e18460f5988d4e24abaeb2f8b1be
Tony <tony.zzx.cn@gmail.com>
2018-05-10 21:58:34 +0800

    Delete experiment

--- experiment/delete_old_data.m
+++
@@ -1,5 +0,0 @@
-% !!!
-% this script will delete all old data!!!
-delete corel5k/*_CSM_fre*;
-delete MSRC/*_CSM_fre*;
-delete Scenes/*_CSM_fre*;
--- experiment/search_subg_txt.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-for /r %%i in (*subg*) do echo %%~nxi | findstr /v /i "mat" | findstr /v /i "txt"
-pause
